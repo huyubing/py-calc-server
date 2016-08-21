14060529e433063004d5e87b4e62974dd81ffd96
pierre <damin-8765@tom.com>
2016-08-21 23:05:16 +0800

    add chapter5.py

---
+++ chapter5.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+print '''Test
+my
+home 
+hz'''
+
+print 'age',42
+
+# as
+import math as math_my
+
+print math_my.sin(30)
+
+print math_my.pi
+
+x, y , z = 1, 2, 'xxx'
+
+
+
+# test
+s = {'name': "damin", "age": 34}
+key, value = s.popitem()
+print key
+print value
--- test.py
+++ test.py
@@ -48,3 +48,4 @@ print a * b
 print a % b
 print a / b
 print a % b
+
