d1823636c1ff09bb68d402f8891a257774c5fbd5
pierre <damin-8765@tom.com>
2016-08-21 23:31:27 +0800

    chapter5.py

--- chapter5.py
+++ chapter5.py
@@ -23,3 +23,45 @@ s = {'name': "damin", "age": 34}
 key, value = s.popitem()
 print key
 print value
+
+
+# use if else elif
+
+name = raw_input("input your name:")
+
+if name == "huyubing":
+    print "your name is :", name
+else:
+    print "your name is ---- ", name
+
+
+# use while
+
+x = 1
+while x < 10:
+    print x
+    x += 1
+
+
+
+print "hello, %s!" % name
+
+# use for loop
+
+words = ['this', 'is', 'an']
+for w in words:
+    print w
+
+numbers = [1, 2, 3, 6 , 9]
+for num in numbers:
+    print num
+
+# for key value
+d = {"name": "damin", "age": 34}
+for key in d:
+    print key, " ----", d[key]
+
+for key, value in d.items():
+    print key, "====:", value
+
+    
