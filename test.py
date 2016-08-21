6db7a3cc4d7f7a88d8cf41f3b3bc8636bc29e505
pierre <damin-8765@tom.com>
2016-08-21 15:17:13 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+# use print
+print 'test python!'
+
+# use x and y
+x = 2
+y = 3
+print x * 3
+
+
+# import math lib
+import math
+
+print math.pi
+
+z = math.pi
+m = z * z
+
+print z
+print str(z)
+
+print len(str(z))
+
+
+# use for and range
+for i in range(0, 10):
+    print i
+
+
+# use break
+print 'break test'
+
+for i in range(10):
+    if i <= 5:
+        print i;
+    else:
+        print 'break'
+        break
